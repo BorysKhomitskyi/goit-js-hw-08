83263e278f7555d2d4bb331f19e2a95322eaf4e4
BorysKhomitskyi <borys2130@gmail.com>
2023-01-29 11:15:28 +0200

    js-hw8

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,43 +5,30 @@ const STORAGE_KEY = 'feedback-form-state';
 const formData = {};
 
 const form = document.querySelector('.feedback-form');
-const textarea = document.querySelector('.feedback-form textarea');
-const email = document.querySelector('.feedback-form input');
 
-form.addEventListener('submit', onFormSubmit);
-textarea.addEventListener('input', throttle(onTextareaInput, 500));
+form.addEventListener('input', throttle(onInputData, 500));
+form.addEventListener('textarea', throttle(onInputData, 500));
+form.addEventListener('submit', onSubmitForm)
 
-populateTextarea();
+dataFromLocalStorage();
 
-form.addEventListener('input', (evt) => {
+function onInputData(evt) {
     formData[evt.target.name] = evt.target.value;
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
-})
+    localStorage.setItem(STORAGE_KEY , JSON.stringify(formData));
+}
 
-function onFormSubmit(evt) { 
+
+function onSubmitForm(evt) {
     evt.preventDefault();
     console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
     evt.currentTarget.reset();
     localStorage.removeItem(STORAGE_KEY);
 }
 
-function onTextareaInput(evt) {
-    const message = evt.target.value;
-    localStorage.setItem(STORAGE_KEY, message);
+function  dataFromLocalStorage() {
+    const data = JSON.parse(localStorage.getItem(STORAGE_KEY));
+    if (data) {
+        email.value = data.email;
+        textarea.value = data.message;
+    }
 }
-
-function populateTextarea() {
-    const savedMessage = localStorage.getItem(STORAGE_KEY);
-
-    if (savedMessage) {
-        textarea.value = savedMessage;
-    }    
-}
-
-function dataFromLocalStorage() {
-  const data = JSON.parse(localStorage.getItem(STORAGE_KEY));
-  if (data) {
-    email.value = data.email;
-    textarea.value = data.message;
-  }
-};
